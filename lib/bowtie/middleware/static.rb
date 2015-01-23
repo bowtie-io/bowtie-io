1b9805bdd54bc57964ea4656ef616fa5c8e87f76
James Kassemi <jkassemi@gmail.com>
2015-01-23 00:03:17 -0700

    Errors served locally from {400,403,404,500}.html
    Permission denied errors are served from local `403.html` given a
    403 response from upstream.
    
    Not found errors are served from local `404.html` given a 404 response
    from upstream.
    
    Any other errors in the 400-499 status code range are handled with the
    `400.html` file. Any errors within the 500-599 status code range are
    handled with the `500.html` file.

--- lib/bowtie/middleware/static.rb
+++ lib/bowtie/middleware/static.rb
@@ -8,20 +8,28 @@ module Bowtie::Middleware
 
     def call(env)
       status, response, headers = @rack_static.call(env)
+      status = status.to_i
 
-      if status == 404
-        env['PATH_INFO'] = '/404.html'
-      elsif status == 403
-        env['PATH_INFO'] = '/403.html'
-      elsif status.between?(500, 599)
-        env['PATH_INFO'] = '/500.html'
-      elsif status.between?(400, 499)
-        env['PATH_INFO'] = '/400.html'
+      if status != 200
+        status, response, headers = @app.call(env)
+        status = status.to_i
+
+        if status == 404
+          env['PATH_INFO'] = '/404.html'
+        elsif status == 403
+          env['PATH_INFO'] = '/403.html'
+        elsif status.between?(500, 599)
+          env['PATH_INFO'] = '/500.html'
+        elsif status.between?(400, 499)
+          env['PATH_INFO'] = '/400.html'
+        else
+          return [status, response, headers]
+        end
+
+        return @rack_static.call(env)
       else
-        return [status, response, headers]
+        [status, response, headers]
       end
-
-      @rack_static.call(env)
     end
   end
 end
