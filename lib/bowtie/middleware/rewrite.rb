e4a4e148055807598fc04273923ec8ba59cbd4dd
James Kassemi <jkassemi@gmail.com>
2015-01-23 16:03:00 -0700

    Header rewrites for development fqdn to local

--- lib/bowtie/middleware/rewrite.rb
+++ lib/bowtie/middleware/rewrite.rb
@@ -1,16 +1,19 @@
+# Rewrites header values referencing the fqdn to make redirects behave
+# nicely without requiring overrides on the BowTie platform.
+
 module Bowtie::Middleware
   class Rewrite
-    def initialize(app)
-      @app = app
+    def initialize(app, options)
+      @app             = app
+      @remote_base_url = "https://#{Bowtie::Settings['project']['fqdn']['development']}"
+      @local_base_url  = "http://#{options['host']}:#{options['port']}"
     end
 
     def call(env)
       status, headers, response = @app.call(env)
-      fqdn     = Bowtie::Settings['project']['fqdn']['development']
-      base_url = "https://#{fqdn}"
 
       headers.each do |k, v|
-        headers[k] = v.gsub(base_url, 'http://localhost:8080')
+        headers[k] = v.gsub(@remote_base_url, @local_base_url)
       end
 
       [status, headers, response]
